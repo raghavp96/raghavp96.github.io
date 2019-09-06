ecc5ee9ca8379608f4895e231e16b8612e848139
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-09-06 10:48:45 -0400

    Changed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Build the app, and commit those changes to source repo
+# Push all committed changes to origin remote, master branch
+git push origin master
+
+# Build the app, and move all built output to raghavp96.github.io folder (it's a repo so it has its own git tracking)
 elm-app build
-cp build-README.md build/
-mv build/build-README.md build/README.md
-git add .
-git commit -m "adding build output to source"
-git push origin master
-# Push the build subtree to the public repo
-git subtree push --prefix=build public master
-# Remove the build directory from the source repo
+mv build/* raghavp96.github.io/
 rm -rf build
+cd raghavp96.github.io
 git add .
-git commit -m "deleting build output from source"
+git commit -m "Adding latest built changes from source."
+# Push changes to origin master for this repo
 git push origin master
+cd ..
