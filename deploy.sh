847c20f8ce4db921c9945357789374e853053c62
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-09-06 09:33:33 -0400

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@
 elm-app build
 cp build-README.md build/
 mv build/build-README.md build/README.md
-ls build
+git add .
+git commit -m "adding build output to source"
 git push origin master
 git subtree push --prefix=build public master
 rm -rf build
 git add .
-git commit -m "deleting build output from soruce"
+git commit -m "deleting build output from source"
 git push origin master
