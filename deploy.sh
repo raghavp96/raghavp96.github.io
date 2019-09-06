486ce2cc2bf08a7d5a1b16c2f9ec40f5f2d478bf
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-09-06 09:38:54 -0400

    Comments in deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# Build the app
+# Build the app, and commit those changes to source repo
 elm-app build
 cp build-README.md build/
 mv build/build-README.md build/README.md
 git add .
 git commit -m "adding build output to source"
 git push origin master
+# Push the build subtree to the public repo
 git subtree push --prefix=build public master
+# Remove the build directory from the source repo
 rm -rf build
 git add .
 git commit -m "deleting build output from source"
