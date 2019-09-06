38caee04b75c66ebee1898e49cf654a4c166a242
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-09-06 11:24:29 -0400

    delete nested submodule and change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-# Push all committed changes to origin remote, master branch
-git push origin master
-
-# Build the app, and move all built output to raghavp96.github.io folder (it's a repo so it has its own git tracking)
+# Build the app
 elm-app build
-cp -r build/* raghavp96.github.io/
-rm -rf build
-cd raghavp96.github.io
 git add .
-git commit -m "Adding latest built changes from source."
-# Push changes to origin master for this repo
+git commit -m "Building latest commits"
+
+# Push all committed changes to origin master, and the build folder to public master
 git push origin master
-cd ..
+git subtree push --prefix build/ public master
